6319105044d143cd1f8b5c8acb29b13a2df1d83c
David Krystall <david.krystall@ontometrics.com>
2017-03-04 12:26:22 -0800

    Initial Commit

---
+++ Pitch Trainer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pitch Trainer
+//
+//  Created by David Krystall on 3/4/17.
+//  Copyright © 2017 David Krystall. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
